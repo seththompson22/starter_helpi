8d769c29250ed965151139e5da17a3c161d6ef05
Seth Thompson <thompsonrussell22@gmail.com>
2024-04-09 11:04:34 -0400

    imported MultipleChoiceQuestion from tasks, to use as a possible question format

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,4 +1,5 @@
 //import React, { useState } from 'react';
+import { Form } from "react-bootstrap";
 import "./App.css";
 //import { Button, Form } from 'react-bootstrap';
 //import { ButtonToPages } from './ButtonToPages';
@@ -7,12 +8,13 @@ export function BasicQuestions() {
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>Seth Thompson</code>
-        </p>
-      </header>
+    <div>
+      <Form>
+        <Form.Group controlId="formBasicQuestions">
+          <Form.Label>Enter Question Here</Form.Label>
+          <Form.Control type="" />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
