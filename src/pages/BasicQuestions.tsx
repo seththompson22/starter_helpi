422e1c60fe8b2f80e6e5fcb9b99c4d3f94645e23
Seth Thompson <thompsonrussell22@gmail.com>
2024-04-09 12:26:52 -0400

    added another question to basic page and planned out question mapping (see comment in BasicQuestions.tsx)

--- src/pages/BasicQuestions.tsx
+++ src/pages/BasicQuestions.tsx
@@ -11,10 +11,23 @@ export function BasicQuestions() {
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
 
   return (
-    <div>
+    <div className="basic-questions-page">
       <NavigationBar></NavigationBar>
       <Form>
+        {/* plan is to modify MultipleChoiceQuestion to be able to enter the question name into a new field called question, 
+        then I can map an input from a text file of the questions for the basic page to create all of the questions in the 
+        form of this type*/}
         <MultipleChoiceQuestion options={["a", "b", "c"]} expectedAnswer="b" />
+        <MultipleChoiceQuestion
+          options={[
+            "Strongly Disagree",
+            "Disagree",
+            "Neither Agree nor Disagree",
+            "Agree",
+            "Strongly Agree",
+          ]}
+          expectedAnswer="Strongly Agree"
+        />
       </Form>
     </div>
   );
