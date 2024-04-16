9918c56324a9e4f7346c6dca0c3ec6fefb984b26
Matthew Holinger <holinger@udel.edu>
2024-04-16 14:51:01 -0400

    fixed imports in my main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import "./styles/App.css";
-import DetailedQuestions from "./pages/DetailedQuestions";
-import { Home } from "./pages/Home";
-import { SignUp } from "./pages/SignUp";
-import { Login } from "./pages/Login";
+//import React, { useState } from 'react';
+//import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+//import { Button, Form } from 'react-bootstrap';
 
-import { HashRouter, Routes, Route } from "react-router-dom";
-import BasicQuestions from "./pages/BasicQuestions";
+//import logo from "./logo.svg";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import DetailedQuestions from './pages/DetailedQuestions';
+import ResponsePage from './ResponsePage'; // Import the ResponsePage component
+import React from 'react';
+
+//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
+let keyData = "";
+const saveKeyData = "MYKEY";
+const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+if (prevKey !== null) {
+  keyData = JSON.parse(prevKey);
+}
 
 function App() {
+  const [key, setKey] = useState<string>(keyData); //for api key input
+  
+  //sets the local storage item to the api key the user inputed
+  function handleSubmit() {
+    localStorage.setItem(saveKeyData, JSON.stringify(key));
+    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+  }
+
+  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
+    setKey(event.target.value);
+  }
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/starter_helpi" element={<Home />} />
-        <Route path="/DetailedQuestions" element={<DetailedQuestions />} />
-        <Route path="/BasicQuestions" element={<BasicQuestions />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </HashRouter>
+<HashRouter>
+  <Routes>
+    <Route path = "/" element = {<Home/>} />
+    <Route path = "/DetailedQuestions" element = {<DetailedQuestions/>} />
+    <Route path = "/ResponsePage" element = {<ResponsePage/>} />
+  </Routes>
+  
+</HashRouter>
   );
 }
 
