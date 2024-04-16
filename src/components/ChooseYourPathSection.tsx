a41037f9e7ac84d0d8044f5ea30431d00d68ea6b
Seth Thompson <thompsonrussell22@gmail.com>
2024-04-16 12:28:03 -0400

    chose temp accent color and made scrolling easier to see

--- src/components/ChooseYourPathSection.tsx
+++ src/components/ChooseYourPathSection.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/ChooseYourPathSection.css";
 
 function ChooseYourPathSection() {
-  return <div className="path-section"></div>;
+  return (
+    <div className="path-section">
+      <h1 className="path-title">CHOOSE YOUR PATH</h1>
+    </div>
+  );
 }
 
 export default ChooseYourPathSection;
