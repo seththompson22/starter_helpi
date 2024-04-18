20347358c1aae70492f04af55ebaadf141b40a66
jordanphotis <jpho@udel.edu>
2024-04-18 14:59:13 -0400

    Got the API button to work and for the API response to be displayed on the website.

--- src/pages/APIButton.tsx
+++ src/pages/APIButton.tsx
@@ -2,27 +2,26 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-import OpenAI from "openai";
-
-
+import {openai} from "./Home"
 
 export function Counter(): JSX.Element {
   function inputAPI() {
     
-    const openai = new OpenAI();
+    //const openai = new OpenAI();
 
     async function computeAPI() {
       const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: "Come up with a rap." }],
         model: "gpt-3.5-turbo",
       });
-    
+      
+      setValue(JSON.stringify(completion.choices[0]));
       console.log(completion.choices[0]);
     
     }
     computeAPI();
   }
-  const [value, setValue] = useState<number>(0);
+  const [value, setValue] = useState<string>("");
   return (
     <div>
       <span>
@@ -32,6 +31,11 @@ export function Counter(): JSX.Element {
       <span>
           <Button onClick={() => inputAPI()}> Here is the API stuff </Button>
       </span>
+      <span>
+        <div>
+            The current value is:{value}
+          </div>
+        </span>
     </div>
   );
 }
