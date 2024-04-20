2ee6e1b980a445b10c45cc78054f2bd81eff7a15
Seth Thompson <thompsonrussell22@gmail.com>
2024-04-19 20:08:52 -0400

    resized content, fixed image for all sizes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,21 +11,26 @@ function HeroSection({
 }): JSX.Element {
   return (
     <div className="hero-container">
-      <video autoPlay loop muted>
-        {" "}
-        <source src={backgroundVideo} type="video/mp4" />
-      </video>
-      <h1>NAVIGATE YOUR CAREER JOURNEY</h1>
-      <p>
-        Discover your path to success today. Take our interactive career quiz
-        and unlock personalized insights tailored just for you. Let's start your
-        journey together!
-      </p>
-      <ScrollToSectionButton
-        className="hero-btn"
-        content="EXPLORE PROFESSIONS"
-        section={sectionToScroll}
-      />
+      <div className="background-video">
+        <video autoPlay loop muted>
+          {" "}
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+        <div className="overlay"></div>
+      </div>
+      <div className="over-text">
+        <h1>NAVIGATE YOUR CAREER JOURNEY</h1>
+        <p>
+          Discover your path to success today. Take our interactive career quiz
+          and unlock personalized insights tailored just for you. Let's start
+          your journey together!
+        </p>
+        <ScrollToSectionButton
+          className="hero-btn"
+          content="EXPLORE PROFESSIONS"
+          section={sectionToScroll}
+        />
+      </div>
     </div>
   );
 }
