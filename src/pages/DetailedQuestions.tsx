0cf7a3c1014a6a3c0567ce64c571085dbde00f28
Matthew Holinger <holinger@udel.edu>
2024-04-29 20:06:14 -0400

    fixed up DQ page format + progress bar implementation

--- src/pages/DetailedQuestions.tsx
+++ src/pages/DetailedQuestions.tsx
@@ -74,115 +74,22 @@ function DetailedQuestions() {
       <Form>
         {questionOptions.map((question, index) => (
           <Form.Group controlId={"question-" + (index + 1)}>
-            <Form.Label className="question-label">
-              Question #{index + 1}:
-            </Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter your answer"
-              onChange={handleChange}
-              className="answer-input" // Add class for answer input
-            />
-          </Form.Group>
+  <Form.Label className="question-label">
+    {"Question #" + (index + 1) + ": " + question}
+  </Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="Enter your answer"
+    onChange={handleChange}
+    className="answer-input" // Add class for answer input
+  />
+</Form.Group>
         ))}
 
         {/* Add some space between questions */}
         <div className="question-space"></div>
       </Form>
 
-      <Container>
-        <Row className="justify-content-center">
-          {" "}
-          {/* Center the questions */}
-          <Col xs={12} md={6}>
-            {" "}
-            {/* Adjust the column width for smaller screens */}
-            {/* Question 1 */}
-            {/* Question 2 */}
-            <Form.Group controlId="question2">
-              <Form.Label className="question-label">Question #2:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 3 */}
-            <Form.Group controlId="question3">
-              <Form.Label className="question-label">Question #3:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 4 */}
-            <Form.Group controlId="question4">
-              <Form.Label className="question-label">Question #4:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 5 */}
-            <Form.Group controlId="question5">
-              <Form.Label className="question-label">Question #5:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 6 */}
-            <Form.Group controlId="question6">
-              <Form.Label className="question-label">Question #6:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 7 */}
-            <Form.Group controlId="question7">
-              <Form.Label className="question-label">Question #7:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-            {/* Add some space between questions */}
-            <div className="question-space"></div>
-            {/* Question 8 */}
-            <Form.Group controlId="question8">
-              <Form.Label className="question-label">Question #8:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your answer"
-                onChange={handleChange}
-                className="answer-input" // Add class for answer input
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-      </Container>
        {/* Render the ProgressBar component */}
        <ProgressBar totalQuestions={questionOptions.length} answeredQuestions={answeredQuestions} />
 
