13c95113e06b1af67673dc410deb7ee1f54e9d46
jordanphotis <jpho@udel.edu>
2024-04-30 12:53:25 -0400

    Fixed the console log error. Hid some of the buttons until the user gets the first response.

--- src/components/APIButton.tsx
+++ src/components/APIButton.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 import {openai, apiQuestions, userAnswers} from "../components/QuestionCard"
@@ -15,13 +15,14 @@ export function APIButton(): JSX.Element {
   // The whole conversation
 
   const [chatLog, setChatLog] = useState<ChatCompletionMessageParam[]>([{ role: "system", content: "You are a career advisor." }, { role: "user", content: "I am trying to figure out what my future career should be. Ask me a list of questions that I can answer." },  ...apiQuestions, ...userAnswers])
-  console.log(chatLog);
+  
 
   async function computeAPI(apiInput: string) {
     // Tries to call the API
     try {
       //const apiMessage: ChatCompletionMessageParam[] = [{ role: "system", content: "You are a career advisor." }, { role: "assistant", content: apiInput }]
-      const apiMessage: ChatCompletionMessageParam[] = [...chatLog, {role: "user", content: apiInput }]
+      const apiMessage: ChatCompletionMessageParam[] = [...chatLog, {role: "user", content: apiInput }];
+      
 
       const completion = await openai.chat.completions.create({
         messages: apiMessage,
@@ -31,8 +32,9 @@ export function APIButton(): JSX.Element {
       setValue(JSON.stringify(completion.choices[0]["message"]["content"]).replace(/\\n/g, "\n"));
       //console.log(completion.choices[0]);
       
-      const apiResponse: ChatCompletionMessageParam[] = [...chatLog, completion.choices[0]["message"]];
-      setChatLog(apiResponse)
+      const apiResponse: ChatCompletionMessageParam[] = [...apiMessage, completion.choices[0]["message"]];
+      setChatLog(apiResponse);
+      
     }
     // Website outputs an error message
     catch (error) {
@@ -40,6 +42,8 @@ export function APIButton(): JSX.Element {
       setValue(JSON.stringify("Error"));
 
     }
+    
+    console.log(chatLog);
   }
   // Hook does what?
   function updateName(event: React.ChangeEvent<HTMLInputElement>) {
@@ -61,18 +65,17 @@ export function APIButton(): JSX.Element {
           <Button onClick={() => computeAPI("")}>Generate your Career Advice</Button>
       </span>
       <span>
-          {/* Button that calls the API on whatever is in the textbox */}
-          <Button onClick={() => computeAPI(apiVal)}>Answer Your Question</Button>
-      </span>
-
-      <span>
-          {/* Button that calls the API to generate a rap */}
-          <Button onClick={() => computeAPI("Come up with a rap.")}> ??? </Button>
-      </span>
-      <span>
-          {/* Outputs whatever the API last said */}
-          The API response is: {value}
-      </span>
+      {value !== "" && 
+      <><span>
+            {/* Button that calls the API on whatever is in the textbox */}
+            <Button onClick={() => computeAPI(apiVal)}>Answer Your Question</Button>
+          </span><span>
+              {/* Button that calls the API to generate a rap */}
+              <Button onClick={() => computeAPI("Come up with a rap.")}> ??? </Button>
+            </span><span>
+              {/* Outputs whatever the API last said */}
+              The API response is: {value}
+            
       {/* Textbox that makes you input your career preferences or whatever */}
       {/*value !== "" && */<Form.Group controlId="apiValue">
         <Form.Label>Enter your career related questions: </Form.Label>
@@ -81,6 +84,9 @@ export function APIButton(): JSX.Element {
                     .
         </Form.Text>
       </Form.Group>}
+      </span></>
+        }
+      </span>
     </div>
   );
 }
