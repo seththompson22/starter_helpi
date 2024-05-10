78eafd43d476ea0dd319ffa89f47ddae0e69cdd5
Seth Thompson <thompsonrussell22@gmail.com>
2024-05-09 23:17:17 -0400

    added hamburger menu with close for all sizes

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,34 +1,63 @@
 //import React, { useState } from 'react';
-import React from "react";
 import "../styles/NavigationBar.css";
 //import { Button, Form } from 'react-bootstrap';
 //import { ButtonToPages } from './ButtonToPages';
+import React, { useState } from "react";
 
 export function NavigationBar() {
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  const [showMenu, setShowMenu] = useState(false);
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setIsSideNavOpen(!isSideNavOpen);
+  };
 
   return (
-    <ul>
-      <div className="left-links">
-        <li>
-          <a className="icon-text nav-item" href="/starter_helpi">
-            JRNY
-          </a>
+    <nav>
+      <ul className="navbar">
+        <div className="left-links">
+          <li>
+            <a className="icon-text nav-item" href="/starter_helpi">
+              JRNY
+            </a>
+          </li>
+        </div>
+        <div className={`right-links ${showMenu ? "show-menu" : ""}`}>
+          <li>
+            <a className="inverted-btn nav-item" href="/starter_helpi/#/signup">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a className="normal-btn nav-item" href="/starter_helpi/#/login">
+              Login
+            </a>
+          </li>
+        </div>
+        <li className="menu-button" onClick={toggleMenu}>
+          <div className={`hamburger-icon ${isSideNavOpen ? "open" : ""}`}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
         </li>
+      </ul>
+      <div className={`side-navigation ${showMenu ? "show" : ""}`}>
+        <ul className="side-bar-content">
+          <li>
+            <a className="" href="/starter_helpi/#/signup">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a className="" href="/starter_helpi/#/login">
+              Login
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className="right-links">
-        <li>
-          <a className="inverted-btn nav-item" href="/starter_helpi/#/signup">
-            Sign Up
-          </a>
-        </li>
-        <li>
-          <a className="normal-btn nav-item" href="/starter_helpi/#/login">
-            Login
-          </a>
-        </li>
-      </div>
-    </ul>
+    </nav>
   );
 }
 
