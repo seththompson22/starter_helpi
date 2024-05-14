d72b10e37ab23b69012a9e33bb5e382b504d3fdd
Matthew Holinger <holinger@udel.edu>
2024-05-13 22:08:10 -0400

    reverted code with all variables declared but not rendering yet

--- src/pages/CareerReport.tsx
+++ src/pages/CareerReport.tsx
@@ -1,54 +1,43 @@
+import "../styles/App.css";
+import "../App";
+import { APIButton } from "../components/APIButton";
+import "../styles/CareerReport.css"
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import CustomFooter from "../components/CustomFooter";
+
 import creative from "../images/creative arts.jpg";
 import education from "../images/education.jpg";
-// Import other images...
+import environment from "../images/environment.jpg";
+import finance from "../images/finance.jpg";
+import healthcare from "../images/healthcare.jpg";
+import hospitality from "../images/hospitality.jpg";
+import manufacturing from "../images/manufacturing.jpg";
+import marketing from "../images/marketing.jpg";
+import technology from "../images/technology.jpg";
 
-// Define an array of images
-const photos: string[] = [creative, education, /* Add other image imports */];
 
-interface CareerReportProps {
-  industry: string; // Define the industry prop
-}
+const photos: string[] = [ creative, education, environment, finance, healthcare, 
+  hospitality, manufacturing, marketing, technology
+];
 
-function CareerReport({ industry }: CareerReportProps) {
-  // Ensure that the value of industry is recognized
-  console.log("Industry:", industry);
 
-  const getPictureForIndustry = () => {
-    switch (industry) {
-      case "Creative Arts":
-        return creative;
-      case "Education":
-        return education;
-      // Handle other industries...
-      default:
-        return ""; // Return an empty string if no industry match
-    }
-  };
-
-  // Ensure that the image path is recognized
-  console.log("Image path:", getPictureForIndustry());
-
+export function CareerReport() {
   return (
     <div className="report-page">
       <NavigationBar />
       <h1>NAVIGATE YOUR CAREER JOURNEY</h1>
-      {/* Conditionally render the picture based on the value of the industry prop */}
-      {industry && <img src={getPictureForIndustry()} alt="Industry" />}
       <div>
-        {/* Add more content here */}
-        <p>
-          Discover your path to success today. Take our interactive career quiz
-          and unlock personalized insights tailored just for you. Let's start your
-          journey together!
-        </p>
+        <APIButton></APIButton>
       </div>
+      <p>
+        Discover your path to success today. Take our interactive career quiz
+        and unlock personalized insights tailored just for you. Let's start your
+        journey together!
+      </p>
       <CustomFooter />
     </div>
   );
 }
 
 export default CareerReport;
-
