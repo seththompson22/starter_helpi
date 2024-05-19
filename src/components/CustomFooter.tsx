0c505700e4be8142b39a8ac65e99e9388b1790fa
Seth Thompson <thompsonrussell22@gmail.com>
2024-05-19 11:42:44 -0400

    added api key check, removed need to hit submit on apikey if its already in localStorage

--- src/components/CustomFooter.tsx
+++ src/components/CustomFooter.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import OpenAI from "openai";
 import "../styles/CustomFooter.css";
-
-export let openai: OpenAI;
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
-const saveKeyData = "MYKEY";
+export const saveKeyData = "MYKEY";
 const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
 if (prevKey !== null) {
   keyData = JSON.parse(prevKey);
@@ -17,19 +14,9 @@ function CustomFooter() {
   const [key, setKey] = useState<string>(keyData); //for api key input
 
   //sets the local storage item to the api key the user inputed
-  function handleSubmit(): OpenAI {
+  function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
-    //window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-    //{} is an object of all of the parameter types.
-    // apiKey lets you manually input the API key.
-    // The ".replace(/"/g, '')" takes off the extra first and last double quotation marks from the key.
-    // The "?." is optional chaining in case the string is null.
-    openai = new OpenAI({
-      apiKey: localStorage.getItem(saveKeyData)?.replace(/"/g, "") || undefined,
-      dangerouslyAllowBrowser: true,
-    });
-
-    return openai;
+    window.location.reload();
   }
 
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
